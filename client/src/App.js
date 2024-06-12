921d078fe535ab67e7463807bfbc9e6509337a95
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-06-12 17:22:27 -0600

    Minor adjustments to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,13 @@
-//import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-
-
-/**
- * 
- * function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
+import './App.css';
 
 const App = () => {
   return (
-
-    /**
-     * You see the <Router> component from react-router-dom, 
-     * In betweeen the <Router> component, you see the <Routes> component,
-     * Inside the <Routes> component, you see the <Route> component.
-     * You can specify the path and the component to render when the path matches.
-     * 
-     */
-    <div className='App'>
+    <div className="App">
       <h1>My React App</h1>
-
-      {/* <Router>
-        <Routes>
-
-          <Route path="/" exact component={HomePage} />
-
-        </Routes>
-      </Router>*/}
-      
-
+      <p>This is a simple test message.</p>
     </div>
-
   );
 };
 
 export default App;
-
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,17 +1,10 @@
-// src/pages/HomePage.js
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 const HomePage = () => {
   return (
     <div>
-      <Header />
-      <main>
-        <h1>Welcome to the Home Page</h1>
-        {/* Your content here */}
-      </main>
-      <Footer />
+      <h2>Welcome to the Home Page</h2>
+      <p>This is the Home Page content.</p>
     </div>
   );
 };
