5380395bf139db21702a925d0a90a2cf0b7de541
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-07-22 14:53:02 -0600

    Added bootstrap to the project

---
+++ client/src/pages/CreateUser.js
@@ -0,0 +1,13 @@
+const CreateUser = () => {
+
+    console.log("Rendering CreateUser Component"); // Debugging statement
+
+    return (
+        <div>
+            <h1>Create User</h1>
+        </div>
+    );
+
+}
+
+export default CreateUser; // Export the CreateUser component
