5ec05d58075521883ec1a7ddb2ccb02abf078578
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-08-24 14:55:56 -0600

    NavBar base is set

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,42 +6,55 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import { useNavigate } from "react-router-dom";
+import styled from "@mui/material/styles/styled";
+
+
+
+const StyledToolbar = styled(Toolbar)({
+  backgroundColor: "#333333",
+  display: "flex",
+  justifyContent: "space-between"
+});
+
+
+const StyledButton = styled(Button)({
+  color: "white"
+
+});
 
 const NavBar = () => {
 
-    const navigate = useNavigate();
-    const handleCreateAccountClick = () => {
-        navigate("/createUser");
-    };
-    const handleSignInClick = () => {
-        navigate("/signIn");
-    };
-    return (
-        
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="sticky">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+  const navigate = useNavigate();
+  const handleCreateAccountClick = () => {
+    navigate("/createUser");
+  };
+  const handleSignInClick = () => {
+    navigate("/signIn");
+  };
+
+
+
+  return (
+
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky">
+        <StyledToolbar>
+          <Button>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} color="white">
               Buzz Planner
             </Typography>
-            <Button color="inherit" onClick={handleSignInClick} sx={{
-                
-            }}>Login</Button>
+          </Button>
+          <StyledButton>
             <Button color="inherit" onClick={handleCreateAccountClick}>Sign Up</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-        
-    );
+            <Button color="inherit" onClick={handleSignInClick} sx={{
+              mr: "10px"
+            }}>Login</Button>
+          </StyledButton>
+        </StyledToolbar>
+      </AppBar>
+    </Box>
+
+  );
 
 };
 
